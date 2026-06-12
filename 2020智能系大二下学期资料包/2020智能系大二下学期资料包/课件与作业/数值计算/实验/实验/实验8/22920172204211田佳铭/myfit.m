7c81f6fae59edf376d0992b7df3b243f74a75883
ruirui6946 <2733936092@qq.com>
2026-06-13 01:06:33 +0800

    Add 2020智能系大二下学期资料包\2020智能系大二下学期资料包\课件与作业\数值计算\实验\实验\实验8\22920172204211田佳铭\myfit.m

---
+++ 2020智能系大二下学期资料包/2020智能系大二下学期资料包/课件与作业/数值计算/实验/实验/实验8/22920172204211田佳铭/myfit.m
@@ -0,0 +1,15 @@
+function p=myfit(x,y,m)
+%函数逼近的多项式拟合
+%p=myfit(x,y,m)
+%x，y为数据向量，m为多项式的次数
+%函数返回p,即多项式的升幂排列系数
+A=zeros(m+1,m+1);
+for i=0:m
+    for j=0:m
+        A(i+1,j+1)=sum(x.^(i+j));
+    end
+    b(i+1)=sum(x.^i.*y);
+end
+    c=A\b';
+    p=c';
+end
