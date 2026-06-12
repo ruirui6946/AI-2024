2ee0c7d4cbcff859154b28abde0db5abb0f7d714
ruirui6946 <2733936092@qq.com>
2026-06-13 00:30:28 +0800

    Add 2020智能系大二下学期资料包\2020智能系大二下学期资料包\课件与作业\汇编语言程序设计\实验\参考实现\各种历史版本\第14周\wmasm-njie3.asm

---
+++ 2020智能系大二下学期资料包/2020智能系大二下学期资料包/课件与作业/汇编语言程序设计/实验/参考实现/各种历史版本/第14周/wmasm-njie3.asm
@@ -0,0 +1,82 @@
+.386
+DATAS SEGMENT
+    ;此处输入数据段代码  
+DATAS ENDS
+
+STACKS SEGMENT
+    ;此处输入堆栈段代码
+STACKS ENDS
+
+CODES SEGMENT
+    ASSUME CS:CODES,DS:DATAS,SS:STACKS
+START:
+NJIE MACRO NA  
+    LOCAL PRINT0
+    MOV BL,NA
+    CMP BL,0
+    JE PRINT0
+    CMP BL,1
+    JE PRINT0
+    MUL EBX
+    DEC BL
+    
+    IF NA	
+    NJIE NA-1
+    ENDIF
+    
+PRINT0:    
+ENDM
+
+    MOV AX,DATAS
+    MOV DS,AX
+    
+    MOV EAX,1
+    MOV EBX,0
+
+    NJIE 5
+    ;MOV DL,AL
+    ;ADD DL,30H
+    ;MOV AH,2
+    ;INT 21H
+    
+    MOV EAX,1111FFFEH
+    ;ADD EAX,1
+    ;MOV EBX,EAX
+    ;MOV EAX,EBX
+    ;DEC EAX
+    
+    MOV DX,10
+    PUSH DX
+    MOV EDX,0
+PUSH_AGAIN:
+    MOV ECX,10
+    DIV ECX
+    MOV EBX,EDX
+    PUSH BX
+    CMP EAX,0
+    JE POP_AGAIN
+    MOV EDX,0
+    JMP PUSH_AGAIN
+    
+POP_AGAIN:
+    POP DX
+    CMP DX,10
+    JE END_OUT
+    ADD DX,30H
+    MOV AH,2
+    INT 21H
+    ;;;
+    MOV AH,1
+    INT 21H
+    ;;;
+    JMP POP_AGAIN
+    
+END_OUT: 
+    ;MOV AH,1
+    ;INT 21H   
+    MOV AH,4CH
+    INT 21H
+    
+CODES ENDS
+    END START
+
