7eeda4d10c3d9de4f2f7435b7ff296b2936aa01c
ruirui6946 <2733936092@qq.com>
2026-06-13 20:47:38 +0800

    Add AI 大三上\AI 大三上\工程文件\操作系统\实验3\real\os-双向有序链表全\threads\threads\threadtest.cc

---
+++ AI 大三上/AI 大三上/工程文件/操作系统/实验3/real/os-双向有序链表全/threads/threads/threadtest.cc
@@ -0,0 +1,134 @@
+// threadtest.cc 
+//	Simple test case for the threads assignment.
+//
+//	Create two threads, and have them context switch
+//	back and forth between themselves by calling Thread::Yield, 
+//	to illustratethe inner workings of the thread system.
+//
+// Copyright (c) 1992-1993 The Regents of the University of California.
+// All rights reserved.  See copyright.h for copyright notice and limitation 
+// of liability and disclaimer of warranty provisions.
+
+#include "copyright.h"
+#include "system.h"
+
+//my
+//#include"dllist.h"
+#include"dllist-driver.cc"
+//#include"dllist.cc"
+//
+
+// testnum is set in main.cc
+int testnum = 1;
+//my
+int threadnum=1;
+int n;
+DLList *dllist;
+//
+
+//----------------------------------------------------------------------
+// SimpleThread
+// 	Loop 5 times, yielding the CPU to another ready thread 
+//	each iteration.
+//
+//	"which" is simply a number identifying the thread, for debugging
+//	purposes.
+//----------------------------------------------------------------------
+
+void
+SimpleThread(int which)
+{
+    int num;
+    
+    for (num = 0; num < 5; num++) {
+	printf("*** thread %d looped %d times\n", which, num);
+        currentThread->Yield();
+    }
+}
+
+//----------------------------------------------------------------------
+// ThreadTest1
+// 	Set up a ping-pong between two threads, by forking a thread 
+//	to call SimpleThread, and then calling SimpleThread ourselves.
+//----------------------------------------------------------------------
+
+void
+ThreadTest1()
+{
+    DEBUG('t', "Entering ThreadTest1");
+
+    Thread *t = new Thread("forked thread");
+
+    t->Fork(SimpleThread, 1);
+    SimpleThread(0);
+}
+
+//my
+void DLListThread(int t)
+{
+    DLList a,b;
+    DLList *insertone,*removeone;
+    insertone=&a;
+    removeone=&b;
+    for(int i=0;i<n;i++)
+    {
+        insertone->Prepend(&i);
+        removeone->Prepend(&i);
+        dllist->Prepend(&i);
+        removen(n,dllist,removeone);
+        insertn(n,dllist,insertone);
+        removeone->Prepend(&i);
+        removen(n,dllist,removeone);
+    }
+    /*for(int i=0;i<n;i++)
+    {
+        insertone->Prepend(&i);
+        removeone->Prepend(&i);
+    }
+    for(int i=0;i<1;i++)
+    {
+        dllist->Prepend(&i);
+    }
+    removen(n,dllist,removeone);
+    insertn(n,dllist,insertone);
+    removen(n,dllist,removeone);
+    insertn(n,dllist,insertone);*/
+}
+
+void ThreadTest2()
+{
+    printf("threadtest2\n");
+    DEBUG('t', "Entering ThreadTest2");
+    dllist=new DLList;
+    for(int i=0;i<threadnum;i++)
+    {
+        Thread *t = new Thread("forked thread");
+        t->Fork(DLListThread,1);
+    }
+    DLListThread(threadnum);
+}
+//
+
+//----------------------------------------------------------------------
+// ThreadTest
+// 	Invoke a test routine.
+//----------------------------------------------------------------------
+
+void
+ThreadTest()
+{
+    switch (testnum) {
+    case 1:
+	ThreadTest1();
+	break;
+    //my
+    case 2:
+        ThreadTest2();
+        break;
+    //
+    default:
+	printf("No test specified.\n");
+	break;
+    }
+}
+
