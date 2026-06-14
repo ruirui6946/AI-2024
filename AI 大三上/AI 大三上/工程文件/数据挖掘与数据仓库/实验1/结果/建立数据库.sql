bb2bf88f81a11807fc4432c93d55aebc402700b5
ruirui6946 <2733936092@qq.com>
2026-06-14 10:27:23 +0800

    Add AI 大三上\AI 大三上\工程文件\数据挖掘与数据仓库\实验1\结果\建立数据库.sql

---
+++ AI 大三上/AI 大三上/工程文件/数据挖掘与数据仓库/实验1/结果/建立数据库.sql
@@ -0,0 +1,68 @@
+create database [student]
+go
+
+use student
+go
+
+/*创建第一个表*/
+create table Course
+(
+    Cno int not null primary key,
+    Cname char(20),
+    Cpno int,
+    Ccredit int,
+);
+
+insert into Course(Cno,Cname,Cpno,Ccredit)
+    values(1,'数据库',5,4)
+insert into Course(Cno,Cname,Ccredit)
+    values(2,'数学',2)
+insert into Course(Cno,Cname,Cpno,Ccredit)
+    values(3,'信息系统',1,4)
+insert into Course(Cno,Cname,Cpno,Ccredit)
+    values(4,'操作系统',6,3)
+insert into Course(Cno,Cname,Cpno,Ccredit)
+    values(5,'数据结构',7,4)
+insert into Course(Cno,Cname,Ccredit)
+    values(6,'数据处理',2)
+insert into Course(Cno,Cname,Cpno,Ccredit)
+    values(7,'PASCAL语言',6,4)
+
+create table Student
+(
+    Sno char(10) not null primary key,
+    Sname char(20),
+    Ssex char(10),
+    Sage int,
+    Sdept char(10)
+);
+
+insert into Student(Sno,Sname,Ssex,Sage,Sdept)
+    values('95001','李勇','男',20,'CS')
+insert into Student(Sno,Sname,Ssex,Sage,Sdept)
+    values('95002','刘晨','女',19,'IS')
+insert into Student(Sno,Sname,Ssex,Sage,Sdept)
+    values('95003','王敏','女',18,'MA')
+insert into Student(Sno,Sname,Ssex,Sage,Sdept)
+    values('95004','张立','男',19,'IS')
+
+create table Sc
+(
+    Sno char(10),
+	Cno int,
+	Grade int,
+	primary key (Sno,Cno),/*组合主码*/
+	foreign key (Sno) references Student(Sno),
+	foreign key (Cno) references Course(Cno)
+);
+
+insert into Sc(Sno,Cno,Grade)
+    values('95001',1,92)
+insert into Sc(Sno,Cno,Grade)
+    values('95001',2,85)
+insert into Sc(Sno,Cno,Grade)
+    values('95001',3,88)
+insert into Sc(Sno,Cno,Grade)
+    values('95002',2,90)
+insert into Sc(Sno,Cno,Grade)
+    values('95002',3,80)
